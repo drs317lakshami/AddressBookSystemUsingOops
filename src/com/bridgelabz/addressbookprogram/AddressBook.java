2d07be59fe65ffa944cd8b59d9e14992414d4785
drs317lakshami <devrajkumar317@gmail.com>
2023-01-22 16:54:38 +0530

    Added :  Ability to create Contacts

---
+++ src/com/bridgelabz/addressbookprogram/AddressBook.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.addressbookprogram;
+
+public class AddressBook {
+
+}
---
+++ src/com/bridgelabz/addressbookprogram/Contacts.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.addressbookprogram;
+
+public class Contacts {
+    private String firstname;
+    private String lastname;
+    private String Address;
+    private String city;
+    private String state;
+    private Double Pin;
+    private Double phoneNumber;
+    private String email;
+}
